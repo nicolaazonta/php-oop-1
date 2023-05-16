b222d59d0bd332a917b77e39bf24c98bf02a0130
nicolaazonta <nicolazonta114@gmail.com>
2023-05-16 19:17:02 +0200

    complete cards style

--- index.php
+++ index.php
@@ -29,13 +29,13 @@ class Movie
     }
 }
 
-$movies[]= new Movie('Interstellar', 'Set in a future where a failing Earth puts humanity on the brink of extinction, it sees an intrepid team of NASA scientists, engineers and pilots attempt to find a new habitable planet, via interstellar travel.', 'Matthew McConaughey', new Genre('horror'));
-   
-$movies[]= new Movie('Avatar', 'Avatar derives from a Sanskrit word meaning "descent," and when it first appeared in English in the late 18th century, it referred to the descent of a deity to the earth—typically, the incarnation in earthly form of Vishnu or another Hindu deity.', 'Sam Worthington', new Genre('horror'));
+$movies[] = new Movie('Interstellar', 'Set in a future where a failing Earth puts humanity on the brink of extinction, it sees an intrepid team of NASA scientists, engineers and pilots attempt to find a new habitable planet, via interstellar travel.', 'Matthew McConaughey', new Genre('horror'));
 
-$movies[]= new Movie('Titanic', 'The Titanic was designed by the Irish shipbuilder William Pirrie and built in Belfast, and was thought to be the world\'s fastest ship. It spanned 883 feet from stern to bow, and its hull was divided into 16 compartments that were presumed to be watertight.', 'Leonardo di Caprio', new Genre('horror'));       
-    
-    
+$movies[] = new Movie('Avatar', 'Avatar derives from a Sanskrit word meaning "descent," and when it first appeared in English in the late 18th century, it referred to the descent of a deity to the earth—typically, the incarnation in earthly form of Vishnu or another Hindu deity.', 'Sam Worthington', new Genre('horror'));
+
+$movies[] = new Movie('Titanic', 'The Titanic was designed by the Irish shipbuilder William Pirrie and built in Belfast, and was thought to be the world\'s fastest ship. It spanned 883 feet from stern to bow, and its hull was divided into 16 compartments that were presumed to be watertight.', 'Leonardo di Caprio', new Genre('horror'));
+
+
 ?>
 
 
@@ -56,12 +56,14 @@ $movies[]= new Movie('Titanic', 'The Titanic was designed by the Irish shipbuild
     <div class="container w-50 pt-5">
 
         <? foreach ($movies as $movie) : ?>
-            <div class="card my-4 p-2">
+            <div class="card border-light my-4 p-2 transparent">
 
 
-                <h1><?= $movie->title ?></h1>
-                <h4 class="text-thin"><?= $movie->description?></h4>
-                <h3><?=$movie->main_actor?></h3>
+                <h1 class=""><?= $movie->title ?></h1>
+                <p class="text-muted"><?= $movie->description ?></p>
+                <figcaption class="mt-3 blockquote-footer">
+                    Main Actor <cite title="Main Actor"><?= $movie->main_actor ?></cite>
+                </figcaption>
 
             </div>
         <? endforeach ?>
